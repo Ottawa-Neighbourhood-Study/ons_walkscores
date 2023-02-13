1222d2b60be5d56f3850622aa2b5a62309a45ff3
Christopher Belanger <christopher@belangeranalytics.com>
2023-02-13 11:08:09 -0500

    finish data run, export to csv and shapefile. NOTE! Richmond neighbourhood seems to be wrong

---
+++ R/functions.R
@@ -0,0 +1,139 @@
+
+get_walkscore <- function(lat, lon, apikey){
+
+  Sys.sleep(.2)
+  url <- sprintf("https://api.walkscore.com/score?format=json&lat=%s&lon=%s&transit=1&bike=1&wsapikey=%s", lat, lon, apikey)
+
+  test <- httr::GET(url)
+
+  httr::content(test) %>%
+    unlist() %>%
+    tibble::as_tibble(rownames = "name") %>%
+    tidyr::pivot_wider(names_from = "name", values_from = "value") %>%
+    dplyr::select(-tidyselect::any_of(c("more_info_icon", "more_info_link", "help_link", "logo_url", "ws_link")))
+
+}
+
+get_walkscore_tidy <- function(df){
+  apikey <- "1a268ff01a519b00ad6ee974caaae248"
+  inputdf <- dplyr::tibble(lat = df$Latitude, lon = df$Longitude, apikey = apikey)
+  walkscores <- purrr::pmap_dfr(inputdf, get_walkscore)
+
+  dplyr::bind_cols(df, walkscores)
+
+}
+
+
+# function gets walkscores for given number of phhs within a set of regions
+process_tranche <- function(tranche, ottawa_phhs, num_to_score = 5, apikey){
+
+  results <- dplyr::tibble()
+
+  for (i in 1:nrow(tranche)) {
+
+    shp <- tranche[i,]
+    message("tranche ", i, "/",nrow(tranche), " - ", shp$ONS_Name)
+
+    shp_info <- dplyr::select(shp, ONS_ID, ONS_Name, ONS_Region) %>% sf::st_set_geometry(NULL)
+
+    # get the phhs that intersect this region, then shuffle them randomly
+    tranche_phhs <-  ottawa_phhs %>%
+      dplyr::filter(as.logical(sf::st_intersects(geometry, shp))) %>%
+      dplyr::slice_sample(n = nrow(.))  %>%
+      dplyr::bind_cols(shp_info)
+
+    # now we get up to num_to_score walkscores. not all phhs will give valid responses
+    j <- 0
+    num_scored <- 0
+    while ((num_scored < num_to_score) & (j < nrow(tranche_phhs))) {
+      j <- j + 1
+      message("walkscore ", j)
+      phh <- tranche_phhs[j,]
+      walkscore <- get_walkscore(lat = phh$Latitude, lon = phh$Longitude, apikey = apikey)
+
+      print(walkscore)
+      # if we get a good response with a non-null status
+      if (!is.null(walkscore$status)) {
+        if (walkscore$status == 1) {
+          num_scored <- num_scored + 1
+          message("good result, have scored ", num_scored, " / ", num_to_score)
+          result <- dplyr::bind_cols(phh, walkscore)
+          results <- rbind(results, result)
+        } # if walkscore$status == 1
+      } # if (!is.null(walkscore$status))
+
+    } # while (num_scored < num_to_score) & (j < nrow(tranche_phhs))
+
+  } # for (i in 1:nrow(tranche))
+
+  return(results)
+}
+
+
+# function gets walkscores for given number of phhs within a set of regions
+process_tranche_errorhandling <- function(tranche, ottawa_phhs, num_to_score = 5, apikey){
+
+  results <- dplyr::tibble()
+
+  for (i in 1:nrow(tranche)) {
+
+    shp <- tranche[i,]
+    message("tranche ", i, "/",nrow(tranche), " - ", shp$ONS_Name)
+
+    shp_info <- dplyr::select(shp, ONS_ID, ONS_Name, ONS_Region) %>% sf::st_set_geometry(NULL)
+
+    # get the phhs that intersect this region, then shuffle them randomly
+    tranche_phhs <-  ottawa_phhs %>%
+      dplyr::filter(as.logical(sf::st_intersects(geometry, shp))) %>%
+      dplyr::slice_sample(n = nrow(.))  %>%
+      dplyr::bind_cols(shp_info)
+
+    # now we get up to num_to_score walkscores. not all phhs will give valid responses
+    j <- 0
+    num_scored <- 0
+    while ((num_scored < num_to_score) & (j < nrow(tranche_phhs))) {
+      j <- j + 1
+      message("walkscore ", j)
+      phh <- tranche_phhs[j,]
+      walkscore <- get_walkscore(lat = phh$Latitude, lon = phh$Longitude, apikey = apikey)
+
+      print(walkscore)
+      # if we get a good response with a non-null status
+      if (!is.null(walkscore$status)) {
+        if (walkscore$status == 1) {
+
+          result <- dplyr::bind_cols(phh, walkscore)
+
+          # using rbind here chokes if a future result doesn't have the same
+          # number of column (e.g. missing bike score) and then subsequent
+          # results also choke with error "Arguments have different crs"
+          # trying <- try (rbind(results, result))
+          # so trying dplyr::bind_rows isntead. can't remember why i didn't in the
+          # first place?
+          if (nrow(results) == 0){
+            trying <- try(result)
+          } else{
+            trying <- try (dplyr::bind_rows(results, result))
+          }
+
+          if (!"try-catch" %in% class (trying)){
+            num_scored <- num_scored + 1
+            message("good result, have scored ", num_scored, " / ", num_to_score)
+            results <- trying
+          } else {
+            message("ERROR with row binding! ")
+            print(head(results))
+            print(result)
+          }
+
+
+        } # if walkscore$status == 1
+      } # if (!is.null(walkscore$status))
+
+    } # while (num_scored < num_to_score) & (j < nrow(tranche_phhs))
+
+  } # for (i in 1:nrow(tranche))
+
+  return(results)
+}
+
--- _targets.R
+++ _targets.R
@@ -9,76 +9,7 @@ tar_option_set(packages = c("tidyverse",
                             "neighbourhoodstudy"
 ))
 
-get_walkscore <- function(lat, lon, apikey){
-
-  Sys.sleep(.2)
-  url <- sprintf("https://api.walkscore.com/score?format=json&lat=%s&lon=%s&transit=1&bike=1&wsapikey=%s", lat, lon, apikey)
-
-  test <- httr::GET(url)
-
-  httr::content(test) %>%
-    unlist() %>%
-    tibble::as_tibble(rownames = "name") %>%
-    tidyr::pivot_wider(names_from = "name", values_from = "value") %>%
-    dplyr::select(-tidyselect::any_of(c("more_info_icon", "more_info_link", "help_link", "logo_url", "ws_link")))
-
-}
-
-get_walkscore_tidy <- function(df){
-  apikey <- "1a268ff01a519b00ad6ee974caaae248"
-  inputdf <- dplyr::tibble(lat = df$Latitude, lon = df$Longitude, apikey = apikey)
-  walkscores <- purrr::pmap_dfr(inputdf, get_walkscore)
-
-  dplyr::bind_cols(df, walkscores)
-
-}
-
-
-# function gets walkscores for given number of phhs within a set of regions
-process_tranche <- function(tranche, ottawa_phhs, num_to_score = 5, apikey){
-
-  results <- dplyr::tibble()
-
-  for (i in 1:nrow(tranche)) {
-
-    shp <- tranche[i,]
-    message("tranche ", i, "/",nrow(tranche), " - ", shp$ONS_Name)
-
-    shp_info <- dplyr::select(shp, ONS_ID, ONS_Name, ONS_Region) %>% sf::st_set_geometry(NULL)
-
-    # get the phhs that intersect this region, then shuffle them randomly
-    tranche_phhs <-  ottawa_phhs %>%
-      dplyr::filter(as.logical(sf::st_intersects(geometry, shp))) %>%
-      dplyr::slice_sample(n = nrow(.))  %>%
-      dplyr::bind_cols(shp_info)
-
-    # now we get up to num_to_score walkscores. not all phhs will give valid responses
-    j <- 0
-    num_scored <- 0
-    while ((num_scored < num_to_score) & (j < nrow(tranche_phhs))) {
-      j <- j + 1
-      message("walkscore ", j)
-      phh <- tranche_phhs[j,]
-      walkscore <- get_walkscore(lat = phh$Latitude, lon = phh$Longitude, apikey = apikey)
-
-      print(walkscore)
-      # if we get a good response with a non-null status
-      if (!is.null(walkscore$status)) {
-        if (walkscore$status == 1) {
-          num_scored <- num_scored + 1
-          message("good result, have scored ", num_scored, " / ", num_to_score)
-          result <- dplyr::bind_cols(phh, walkscore)
-          results <- rbind(results, result)
-        } # if walkscore$status == 1
-      } # if (!is.null(walkscore$status))
-
-    } # while (num_scored < num_to_score) & (j < nrow(tranche_phhs))
-
-  } # for (i in 1:nrow(tranche))
-
-  return(results)
-}
-
+source("./R/functions.R")
 
 list(
   #################################### -
@@ -96,6 +27,159 @@ list(
  targets::tar_target(tranche1Walkscores,
                        process_tranche(tranche = tranche1, ottawa_phhs = ottawa_phhs, num_to_score = 100, apikey = apikey )
                      ),
+
+ targets::tar_target(tranche2,
+                     ons_gen3 %>%
+                       dplyr::filter(ONS_Region == "OTTAWA") %>%
+                       dplyr::filter(!ONS_ID %in% tranche1$ONS_ID) %>%
+                       dplyr::slice(1:20) %>%
+                       sf::st_make_valid()
+  ),
+
+ targets::tar_target(tranche2Walkscores,
+                     process_tranche(tranche = tranche2, ottawa_phhs = ottawa_phhs, num_to_score = 100, apikey = apikey )
+ ),
+
+ targets::tar_target(tranche3,
+                     ons_gen3 %>%
+                       dplyr::filter(ONS_Region == "OTTAWA") %>%
+                       dplyr::filter(!ONS_ID %in% tranche1$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche2$ONS_ID) %>%
+                       dplyr::slice(1:45) %>%
+                       sf::st_make_valid()
+                     ),
+
+ targets::tar_target(tranche3Walkscores,
+                     process_tranche(tranche = tranche3, ottawa_phhs = ottawa_phhs, num_to_score = 100, apikey = apikey )
+ ),
+
+ targets::tar_target(tranche4,
+                     ons_gen3 %>%
+                       dplyr::filter(ONS_Region == "OTTAWA") %>%
+                       dplyr::filter(!ONS_ID %in% tranche1$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche2$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche3$ONS_ID) %>%
+                       dplyr::slice(1:10) %>%
+                       sf::st_make_valid()
+                     ),
+
+ targets::tar_target(tranche5,
+                     ons_gen3 %>%
+                       dplyr::filter(ONS_Region == "OTTAWA") %>%
+                       dplyr::filter(!ONS_ID %in% tranche1$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche2$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche3$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche4$ONS_ID) %>%
+                       dplyr::slice(1:7) %>%
+                       sf::st_make_valid()
+ ),
+
+ targets::tar_target(tranche6,
+                     ons_gen3 %>%
+                       dplyr::filter(ONS_Region == "OTTAWA") %>%
+                       dplyr::filter(!ONS_ID %in% tranche1$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche2$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche3$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche4$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche5$ONS_ID) %>%
+                       dplyr::slice(1:10) %>%
+                       sf::st_make_valid()
+ ),
+
+ # NOTE!! ERROR IN RICHMOND NEIHGBOURHOOD!!
+ targets::tar_target(tranche7,
+                     ons_gen3 %>%
+                       dplyr::filter(ONS_Region == "OTTAWA") %>%
+                       dplyr::filter(!ONS_ID %in% tranche1$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche2$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche3$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche4$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche5$ONS_ID) %>%
+                       dplyr::filter(!ONS_ID %in% tranche6$ONS_ID) %>%
+                       dplyr::filter(ONS_Name != "RICHMOND") %>%
+                       sf::st_make_valid()
+ ),
+
+ targets::tar_target(tranche4Walkscores,
+                     process_tranche_errorhandling(tranche = tranche4, ottawa_phhs = ottawa_phhs, num_to_score = 100, apikey = apikey )
+ ),
+
+ targets::tar_target(tranche5Walkscores,
+                     process_tranche_errorhandling(tranche = tranche5, ottawa_phhs = ottawa_phhs, num_to_score = 100, apikey = apikey )
+ ),
+
+ targets::tar_target(tranche6Walkscores,
+                     process_tranche_errorhandling(tranche = tranche6, ottawa_phhs = ottawa_phhs, num_to_score = 100, apikey = apikey )
+ ),
+
+ targets::tar_target(tranche7Walkscores,
+                     process_tranche_errorhandling(tranche = tranche7, ottawa_phhs = ottawa_phhs, num_to_score = 100, apikey = apikey )
+ ),
+
+ targets::tar_target(walkscore_points,
+                     dplyr::bind_rows(tranche1Walkscores,
+                                      tranche2Walkscores,
+                                      tranche3Walkscores,
+                                      tranche4Walkscores,
+                                      tranche5Walkscores,
+                                      tranche6Walkscores,
+                                      tranche7Walkscores) %>%
+                       dplyr::filter(ONS_Region == "OTTAWA")
+                     ),
+
+ targets::tar_target(walkscore_regions,
+                     walkscore_points %>%
+                       sf::st_set_geometry(NULL) %>%
+                       dplyr::group_by(ONS_ID, ONS_Name) %>%
+                     dplyr::summarise(walkscore_mean = mean(as.numeric(walkscore))) %>%
+                       dplyr::left_join(ons_gen3, .) %>%
+                       dplyr::filter(ONS_Region == "OTTAWA")
+                     ),
+
+
+ targets::tar_target(walkscore_points_plot,
+                     ggplot2::ggplot(walkscore_points) +
+                       ggplot2::geom_sf(ggplot2::aes(colour = as.numeric(walkscore)))
+                     ),
+
+ targets::tar_target(walkscore_points_fancy,
+                     walkscore_points_plot +
+                       theme_void() +
+                       theme(legend.position="none", plot.background = element_rect(fill="black")) +
+                       scale_colour_viridis_c()),
+
+
+ targets::tar_target(walkscore_regions_plot,
+                     ggplot2::ggplot(walkscore_regions) +
+                       ggplot2::geom_sf(ggplot2::aes(fill = as.numeric(walkscore_mean)), colour = NULL) +
+                       ggplot2::theme_void() +
+                       ggplot2::theme(legend.position="none", plot.background = element_rect(fill="black")) +
+                       ggplot2::scale_fill_viridis_c()
+ ),
+
+ targets::tar_target(save_results,
+                    {
+                      # points: save sf and csv files
+                      walkscore_points %>%
+                        sf::write_sf(sprintf("outputs/walkscore-points-%s.shp", Sys.Date())) %>%
+                        sf::st_set_geometry(NULL) %>%
+                        readr::write_csv(sprintf("outputs/walkscore-points-%s.csv", Sys.Date()))
+
+                      # regions: save sf and csv files
+                      walkscore_regions %>%
+                        sf::write_sf(sprintf("outputs/walkscore-hoods-%s.shp", Sys.Date())) %>%
+                        sf::st_set_geometry(NULL) %>%
+                        readr::write_csv(sprintf("outputs/walkscore-hoods-%s.csv", Sys.Date()))
+
+                      # readr::write_csv(sf::st_set_geometry(walkscore_points, NULL), sprintf("outputs/walkscore-points-%s.csv", Sys.Date))
+                      # readr::write_csv(sf::st_set_geometry(walkscore_regions, NULL), sprintf("outputs/walkscore-regions-%s.csv", Sys.Date))
+                      #
+                      # # save shapefiles
+                      # sf::write_sf(walkscore_points, sprintf("outputs/walkscore-points-%s.shp", Sys.Date))
+                      # sf::write_sf(walkscore_regions, sprintf("outputs/walkscore-regions-%s.s.shp", Sys.Date))
+
+                    }
+                    ),
 
  NULL
 )
